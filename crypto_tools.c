079ebb4a261a6454fe31960c5f99fdbe93479eed
saba <saba.eskandarian@gmail.com>
2023-06-22 14:05:08 -0400

    switch crypto_tools to use aes128 which is what the rest of the code was already assuming.

--- crypto_tools.c
+++ crypto_tools.c
@@ -56,7 +56,7 @@ int ccAEEnc(uint8_t* enc_key, uint8_t* msg, int msg_len, uint8_t* c1_ct, uint8_t
             handleErrors();
 
         /* Initialise the encryption operation. */
-        if(1 != EVP_EncryptInit_ex(ctx, EVP_aes_256_gcm(), NULL, enc_key, iv))
+        if(1 != EVP_EncryptInit_ex(ctx, EVP_aes_128_gcm(), NULL, enc_key, iv))
             handleErrors();
 
         /*
@@ -112,7 +112,7 @@ int ccAEDec(uint8_t* enc_key, uint8_t* c1_ct, int c1_ct_len, uint8_t* c1_tag, ui
         handleErrors();
 
     /* Initialise the decryption operation. */
-    if(!EVP_DecryptInit_ex(ctx, EVP_aes_256_gcm(), NULL, enc_key, iv))
+    if(!EVP_DecryptInit_ex(ctx, EVP_aes_128_gcm(), NULL, enc_key, iv))
         handleErrors();
 
     /*
